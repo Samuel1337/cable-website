4e04bd286f5af77cdb112f691aeada296a677a17
Samuel Martins <smbra33@gmail.com>
2022-12-11 18:55:13 -0800

    finish technical pictures section

--- frontend/src/components/Technical.js
+++ frontend/src/components/Technical.js
@@ -1,46 +1,27 @@
 import React from 'react';
+import './Technical.css';
 import './Cards.css';
 import CardItem from './CardItem';
 
 function Technical() {
   return (
-    <div className='cards' id="technical">
-      <h1>TECHNICAL</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
+    <div className='technical' id="technical">
+      <div className='technical__container'>
+        <div className='technical__wrapper'>
+          <ul className='technical__items'>
+            <CardItem src='images/tech-9.png' />
+            <CardItem src='images/tech-11.png' />
           </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
-            />
-            <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
-            />
+          <h1 className='technical__title'>BRACE YOURSELF FOR BREATHTAKING VISUALS</h1>
+          <ul className='technical__items'>
+            <CardItem src='images/tech-12.png' />
+            <CardItem src='images/tech-16.png' />
+            <CardItem src='images/tech-15.png' />
+          </ul>
+          <h1 className='technical__title'>CUTTING-EDGE TECHNOLOGY MEETS THE FINE ARTS</h1>
+          <ul className='technical__items'>
+            <CardItem src='images/tech-14.png' />
+            <CardItem src='images/tech-19.png' />
           </ul>
         </div>
       </div>
