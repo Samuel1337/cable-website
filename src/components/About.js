42ce9e8b1b83a86e5613ac1b85f2fe0b9c836569
Samuel Martins <smbra33@gmail.com>
2023-01-08 19:34:44 -0800

    redo about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Cards.css';
 import './About.css';
 import picture from '../images/Golden_Gate_Bridge_construction_42.jpg'
+import { Fade } from 'react-reveal';
 
 function About() {
   return (
@@ -10,7 +11,9 @@ function About() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <div className='about__video'>
-              <img className='about__img' src={picture} alt="" />
+              <Fade bottom distance="10%">
+                <img className='about__img' src={picture} alt="" />
+              </Fade>
             {/* <iframe 
               className='about__iframe'
               src="https://www.youtube.com/embed/ZUxfScnbf-w"
@@ -21,23 +24,27 @@ function About() {
             </iframe> */}
             </div>
             <div className='about__text'>
-              <h1>A NEW GENERATION</h1>
-              <h1>A NEW EXPLANATION</h1>
-              <br></br>
-              <br></br>
-              <p>
-                The Golden Gate Bridge is the symbol of Impossible Dreams.
-                Against all odds, the bridge that couldn't be built stands firm until today.
-                Our mission is to inspire contemporary audiences to believe that
-                <span className='italic'> Impossible Dreams are Possible </span>
-                by retelling this forgotten adventure.
-              </p>
+              <Fade bottom distance="10%">
+                <h1>A NEW GENERATION</h1>
+                <h1>A NEW EXPLANATION</h1>
+                <br></br>
+                <br></br>
+                <p>
+                  The Golden Gate Bridge is the symbol of Impossible Dreams.
+                  Against all odds, the bridge that couldn't be built stands firm until today.
+                  Our mission is to inspire contemporary audiences to believe that
+                  <span className='italic'> Impossible Dreams are Possible </span>
+                  by retelling this forgotten adventure.
+                </p>
+              </Fade>
             </div>
           </ul>
           <div className='about__subtitle'>
-            <p>NOVEL</p>
-            <p>MUSICAL</p>
-            <p>VR EXPERIENCE</p>
+            <Fade bottom distance="10%">
+              <p>NOVEL</p>
+              <p>MUSICAL</p>
+              <p>VR EXPERIENCE</p>
+            </Fade>
           </div>          
         </div>
       </div>
--- src/components/Novel.js
+++ src/components/Novel.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Fade } from 'react-reveal';
 import './Cards.css';
 import './Novel.css';
 
-function Novel() {
+function Novel() {  
+
   return (
     <div className='novel' id="novel">
-      <h1>NOVEL</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
+          
           <ul className='cards__items'>
             <div className='novel__video'>
-            <iframe
+            {/* <iframe
               className='novel__iframe'
               src="https://www.youtube.com/embed/ZUxfScnbf-w"
               title="San Francisco"
               frameBorder="0"
               allowFullScreen
             >
-            </iframe>
+            </iframe> */}
             </div>
             <div className='novel__text'>
-              <p>
-                We have been developing an underlying novel that will be the foundation for all our creative expressions.
-              </p>
-              <p>
-                The story focuses on Tony who loses his family to the Oklahoma Dust Bowl and joins the Great Migration to California in search of a new beginning.
-              </p>
-              <p>
-                In the magnificent city of San Francisco he embarks upon a journey of risk and danger, friendship and betrayal, love lost and found, as he works on the construction of the Golden Gate Bridge, the Impossible Dream.
-              </p>
+              <Fade bottom distance="10%">
+                <h1>CABLE, THE NOVEL</h1>
+              <br></br>
+                <p>
+                  We have been developing an underlying novel that will be the foundation for all our creative expressions.
+                </p>
+              <br></br>
+                <p>
+                  The story focuses on Tony who loses his family to the Oklahoma Dust Bowl and joins the Great Migration to California in search of a new beginning.
+                </p>
+              <br></br>
+                <p>
+                  In the magnificent city of San Francisco he embarks upon a journey of risk and danger, friendship and betrayal, love lost and found, as he works on the construction of the Golden Gate Bridge,<span className='italic'> the Impossible Dream.</span>
+                </p>
+              </Fade>
             </div>
           </ul>
           
